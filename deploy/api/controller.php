fdb5d40392466fcf6801ba33ad55d2deebd77bc7
ybokhari <younes.bokhari@outlook.com>
2024-02-01 15:13:06 +0100

    Begin tp02

---
+++ deploy/api/controller.php
@@ -0,0 +1,68 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+	function optionsCatalogue (Request $request, Response $response, $args) {
+	    
+	    // Evite que le front demande une confirmation à chaque modification
+	    $response = $response->withHeader("Access-Control-Max-Age", 600);
+	    
+	    return addHeaders ($response);
+	}
+
+	function hello(Request $request, Response $response, $args) {
+	    $array = [];
+	    $array ["nom"] = $args ['name'];
+	    $response->getBody()->write(json_encode ($array));
+	    return $response;
+	}
+	
+	function  getSearchCalatogue (Request $request, Response $response, $args) {
+	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+		
+	   $response->getBody()->write($flux);
+	   
+	    return addHeaders ($response);
+	}
+
+	// API Nécessitant un Jwt valide
+	function getCatalogue (Request $request, Response $response, $args) {
+	    $flux = '[{"titre":"linux","ref":"001","prix":"20"},{"titre":"java","ref":"002","prix":"21"},{"titre":"windows","ref":"003","prix":"22"},{"titre":"angular","ref":"004","prix":"23"},{"titre":"unix","ref":"005","prix":"25"},{"titre":"javascript","ref":"006","prix":"19"},{"titre":"html","ref":"007","prix":"15"},{"titre":"css","ref":"008","prix":"10"}]';
+	    
+	    $response->getBody()->write($flux);
+	    
+	    return addHeaders ($response);
+	}
+
+	function optionsUtilisateur (Request $request, Response $response, $args) {
+	    
+	    // Evite que le front demande une confirmation à chaque modification
+	    $response = $response->withHeader("Access-Control-Max-Age", 600);
+	    
+	    return addHeaders ($response);
+	}
+
+	// API Nécessitant un Jwt valide
+	function getUtilisateur (Request $request, Response $response, $args) {
+	    
+	    $payload = getJWTToken($request);
+	    $login  = $payload->userid;
+	    
+		$flux = '{"nom":"martin","prenom":"jean"}';
+	    
+	    $response->getBody()->write($flux);
+	    
+	    return addHeaders ($response);
+	}
+
+	// APi d'authentification générant un JWT
+	function postLogin (Request $request, Response $response, $args) {   
+	    
+		$flux = '{"nom":"martin","prenom":"jean"}';
+	    
+	    $response = createJwT ($response);
+	    $response->getBody()->write($flux );
+	    
+	    return addHeaders ($response);
+	}
+
